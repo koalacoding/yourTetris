8a19cb33962564c4d819c7c1891a1ccb8cfe0626
groskoala <hadrien7845@gmail.com>
2015-11-08 03:26:54 +0100

    Update default canvas and grid size.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   var context = $('#canvas')[0].getContext('2d');
 
-	var grid = new Grid(175, 10, 250, 16, context);
+	var grid = new Grid(252, 10, 360, 16, context);
 	grid.initializeGridData();
 	grid.drawGrid();
 
