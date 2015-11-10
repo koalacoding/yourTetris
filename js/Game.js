04bda7f8dd937aca02d7bd0666828317176a8888
koalacoding <koalacoding@net-c.fr>
2015-11-10 14:25:10 +0100

    Deletes unused function restartGame in Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,75 +1,66 @@
 function Game() {
-  var that = this;
+  
+var that = this;
 
-  var fadeHandler = new FadeHandler();
+var fadeHandler = new FadeHandler();
 
-  var context = $('#canvas')[0].getContext('2d');
+var context = $('#canvas')[0].getContext('2d');
 
-  var score = new Score();
-  this.grid = new Grid(252, 10, 360, 16, context, that, fadeHandler, score);
+var score = new Score();
+this.grid = new Grid(252, 10, 360, 16, context, that, fadeHandler, score);
 
-  this.gameOver = false;
-
-
-  /*---------------------------------------
-  -----------------------------------------
-  --------------GAME HANDLERS--------------
-  -----------------------------------------
-  ---------------------------------------*/
+this.gameOver = false;
 
 
-    /*--------------------
-    ------START GAME------
-    --------------------*/
+/*---------------------------------------
+-----------------------------------------
+--------------GAME HANDLERS--------------
+-----------------------------------------
+---------------------------------------*/
 
-    this.startGame = function() {
-      $('#menu').fadeOut(function() {
-        that.gameOver = false;
 
-        $('#game').fadeOut(function() { // If the user was already playing a game.
-          $('#score span').text('0'); // Setting the score to zero.
-        });
+  /*--------------------
+  ------START GAME------
+  --------------------*/
 
-        context.clearRect(0, 0, 252, 360); // Deleting everything drawn on the canvas.
-        that.grid.initialize();
+  this.startGame = function() {
+    $('#menu').fadeOut(function() {
+      that.gameOver = false;
 
-        $('#game').fadeIn();
+      $('#game').fadeOut(function() { // If the user was already playing a game.
+        $('#score span').text('0'); // Setting the score to zero.
       });
-    }
 
-    /*--------------------------
-    ------PROPOSE NEW GAME------
-    --------------------------*/
+      context.clearRect(0, 0, 252, 360); // Deleting everything drawn on the canvas.
+      that.grid.initialize();
 
-    this.proposeNewGame = function(fadeHandler) {
-      $( "#dialog-confirm" ).attr('title', 'Game over');
-  		$( ".ui-dialog-title" ).text('Game over');
-      $( "#dialog-confirm p" ).text('Do you want to play again ?');
-  		$( "#dialog-confirm" ).dialog({
-  			resizable: false,
-  			height: 150,
-  			modal: true,
-  			buttons: {
-  				Yes: function() {
-            that.startGame(fadeHandler);
-  					$( this ).dialog( "close" );
-  				},
-  				No: function() {
-  					$( this ).dialog( "close" );
-            fadeHandler.fadeCanvasOut();
-  				}
-  			}
-  		});
-    }
+      $('#game').fadeIn();
+    });
+  }
 
-    /*----------------------
-    ------RESTART GAME------
-    ----------------------*/
+  /*--------------------------
+  ------PROPOSE NEW GAME------
+  --------------------------*/
 
-    /*this.restartGame = function() {
-      that.grid.gridData = [];
-      that.grid.initializeGridData();
-      that.grid.drawAllTetrominoSquares();
-      this.gameOver = false;
-    }*/
+  this.proposeNewGame = function(fadeHandler) {
+    $( "#dialog-confirm" ).attr('title', 'Game over');
+		$( ".ui-dialog-title" ).text('Game over');
+    $( "#dialog-confirm p" ).text('Do you want to play again ?');
+		$( "#dialog-confirm" ).dialog({
+			resizable: false,
+			height: 150,
+			modal: true,
+			buttons: {
+				Yes: function() {
+          that.startGame(fadeHandler);
+					$( this ).dialog( "close" );
+				},
+				No: function() {
+					$( this ).dialog( "close" );
+          fadeHandler.fadeCanvasOut();
+				}
+			}
+		});
+  }
+
 }
